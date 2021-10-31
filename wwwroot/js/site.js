b69a9a45ee07f04bf93a7a9361998dcaee218b7d
Ailton Alves <alvs.ailton@gmail.com>
2021-10-31 12:02:44 -0300

    sidebar menu responsive mobile

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.addEventListener('DOMContentLoaded', event => {
 
-// Write your JavaScript code.
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
