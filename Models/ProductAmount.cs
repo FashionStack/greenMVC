322d5c3e8d72b2629ce728959d8f75f7f1bafbc3
Wallace Silva <wleitte@outlook.com>
2021-11-02 18:24:10 -0300

    Implementação do Método de Gerenciamento de estoque e ajustes gerais.

---
+++ Models/ProductAmount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenMVC.Models
+{
+    public class ProductAmount
+    {
+        public long ProductId { get; set; }
+        public int Amount { get; set; }
+    }
+}
--- Pages/ProductPages/Delete.cshtml.cs
+++ Pages/ProductPages/Delete.cshtml.cs
@@ -7,20 +7,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using GreenMVC.Context;
 using GreenMVC.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace GreenMVC.Pages.ProductPages
 {
     public class DeleteModel : PageModel
     {
         private readonly GreenMVC.Context.GreenStockContextMVC _context;
+        private IConfiguration Configuration;
 
-        public DeleteModel(GreenMVC.Context.GreenStockContextMVC context)
+        public DeleteModel(GreenMVC.Context.GreenStockContextMVC context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         [BindProperty]
         public Product Product { get; set; }
+
+        string baseUrl => Configuration.GetConnectionString("ApiUrl");
+
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -29,31 +38,46 @@ namespace GreenMVC.Pages.ProductPages
                 return NotFound();
             }
 
-            Product = await _context.Product.FirstOrDefaultAsync(m => m.ProductId == id);
-
-            if (Product == null)
+            using (var client = new HttpClient())
             {
-                return NotFound();
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/Products/" + id);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Product = JsonConvert.DeserializeObject<Product>(result);
+                }
             }
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (id == null)
+            using (var client = new HttpClient())
             {
-                return NotFound();
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client
+                    .DeleteAsync("api/Products/" + id);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    //Produtos/Index
+                    return RedirectToPage("./Index");
+                }
+                else
+                {
+                    return RedirectToPage("./Create");
+                }
             }
-
-            Product = await _context.Product.FindAsync(id);
-
-            if (Product != null)
-            {
-                _context.Product.Remove(Product);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
         }
     }
 }
