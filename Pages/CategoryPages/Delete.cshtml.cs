478028164f0aa97ee37810877cf8b70ed5ecbaac
Wallace Silva <wleitte@outlook.com>
2021-11-02 22:58:45 -0300

    Corrigido o DeleteModel de Categoria, que não estava consumindo a API.

--- Pages/CategoryPages/Delete.cshtml.cs
+++ Pages/CategoryPages/Delete.cshtml.cs
@@ -7,20 +7,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using GreenMVC.Context;
 using GreenMVC.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace GreenMVC.Pages.CategoryPages
 {
     public class DeleteModel : PageModel
     {
-        private readonly GreenMVC.Context.GreenStockContextMVC _context;
-
-        public DeleteModel(GreenMVC.Context.GreenStockContextMVC context)
+        private IConfiguration Configuration;
+        public DeleteModel(IConfiguration configuration)
         {
-            _context = context;
+            Configuration = configuration;
         }
 
         [BindProperty]
         public Category Category { get; set; }
+
+        string baseUrl => Configuration.GetConnectionString("ApiUrl");
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -29,31 +34,45 @@ namespace GreenMVC.Pages.CategoryPages
                 return NotFound();
             }
 
-            Category = await _context.Category.FirstOrDefaultAsync(m => m.CategoryId == id);
-
-            if (Category == null)
+            using (var client = new HttpClient())
             {
-                return NotFound();
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/Categories/" + id);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Category = JsonConvert.DeserializeObject<Category>(result);
+                }
             }
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (id == null)
+            using (var client = new HttpClient())
             {
-                return NotFound();
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client
+                    .DeleteAsync("api/Categories/" + id);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToPage("./Index");
+                }
+                else
+                {
+                    return RedirectToPage("./Create");
+                }
             }
-
-            Category = await _context.Category.FindAsync(id);
-
-            if (Category != null)
-            {
-                _context.Category.Remove(Category);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
         }
     }
 }
